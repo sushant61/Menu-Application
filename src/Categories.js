c2e038e2344916b947fb11c4cfd85dda5fa9125b
Sushant Prasad <sushantprasad1234@gmail.com>
2022-09-08 20:35:33 +0530

    committed

---
+++ src/Categories.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Categories = ({ categories, filteritems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            key={index}
+            onClick={() => filteritems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
